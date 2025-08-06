44c347b8e4ace15719239d501d8bad47db963b9f
Gadius <gadlapaz@hotmail.com>
2025-08-05 18:15:07 -0700

    Auth, Users, Profiles resources created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,44 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ProfilesModule } from './profiles/profiles.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, ProfilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+/*
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { AuthModule } from './auth/auth.module'
+import { UsersModule } from './users/users.module'
+import { ProfilesModule } from './profiles/profiles.module'
+import { AuthModule } from './auth/auth.module';
+import { ProfilesModule } from './profiles/profiles.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'nest_user',
+      password: 'strongpass',
+      database: 'nest_auth',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AuthModule,
+    UsersModule,
+    ProfilesModule,
+  ],
+})
+export class AppModule {}
+*/
